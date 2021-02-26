d502d0c6050a0361cb7d742184dbb81107a44cde
Andy <webmaster@RealityRipple.com>
2021-02-25 18:47:03 -0800

    AMO Version for SeaMonkey
    Technically addons.thunderbird.net, but same difference.

--- defaults/preferences/gaiaformat.js
+++ defaults/preferences/gaiaformat.js
@@ -1,4 +1,4 @@
-pref("extensions.{F4999074-2E3B-5531-8A05-0BA42DDB3A58}.description", "chrome://gaiaformat/locale/gaiaformat.properties");
+pref("extensions.{B782732B-3252-95C0-8AF4-526E6E00C898}.description", "chrome://gaiaformat/locale/gaiaformat.properties");
 pref("extensions.gaiaformat.Formats", 0);
 pref("extensions.gaiaformat.UnsafeAlert", true);
 pref("extensions.gaiaformat.Favored", 'default');
